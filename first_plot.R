b751b3459ffff1798e788687b311d642b37f06ef
Bilal <mbilalnust@gmail.com>
2020-08-25 12:27:17 +0900

    first commit

---
+++ first_plot.R
@@ -0,0 +1,9 @@
+dataFile <- read.csv("C:/Users/tops/Desktop/coursera project/household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+data <- read.table(dataFile)
+subSetData <- dataFile[dataFile$Date %in% c("1/2/2007","2/2/2007") ,]
+
+str(subSetData)
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
